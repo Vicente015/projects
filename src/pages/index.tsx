10a8c423d55a9199b5366feb27aac1c23d611ad1
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-03-05 18:06:09 +0000

    :sparkles: first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Icons from '../components/Icons'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>AnnounceIt</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="min-h-screen font-sans bg-[#2D283C]">
+        <div className="flex justify-center m-auto p-24 bg-blurple-500">
+          <div className="logo">
+            <p>logito</p>
+          </div>
+          <h1 className="text-4xl text-white font-bold">AnnounceIt</h1>
+        </div>
+        <div className="flex flex-col mt-2 p-3 max-w-[70ch] m-auto">
+          <section>
+            <h2 className="text-xl font-semibold text-gray-100">Publishes announcements in various languages</h2>
+            <p className="text-sm text-gray-200">
+            AnnounceIt is a Discord bot designed to facilitate communication in servers with a variety of languages. It allows posting messages with support for multiple languages and provides a button to switch between languages, ensuring that all users can understand important announcements.
+            </p>
+            <p className="text-lg font-semibold text-gray-100 mt-2">Features:</p>
+            <ul className="list-disc list-inside">
+              <li className="text-gray-200">title</li>
+            </ul>
+          </section>
+          <section>
+            <h2>Get involved</h2>
+            <div className="grid">
+              <div className="container flex">
+                <div className="svg"></div>
+                <div className="right">
+                  <h3>Get the app</h3>
+                  <p>Lorem</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
