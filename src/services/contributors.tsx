9e22f44045da87e2d84784d50b4c991e5a494610
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-03-15 12:04:23 +0000

    ✨ Improve contributors css & add user socials (icons wip)

--- src/services/contributors.tsx
+++ src/services/contributors.tsx
@@ -1,11 +1,16 @@
 import { z } from 'zod'
-
-const apiURL = (repoName: string) => `https://api.github.com/repos/Vicente015/${repoName}/contributors`
 const GH_API_TOKEN = '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'
+
+const contributorsURL = (repoName: string) => `https://api.github.com/repos/Vicente015/${repoName}/contributors`
+const socialsURL = (username: string) => `https://api.github.com/users/${username}/social_accounts`
 
 const ContributorSchema = z.object({
   avatarUrl: z.string().url(),
   contributions: z.number(),
+  socialAccounts: z.array(z.object({
+    provider: z.string(),
+    url: z.string().url()
+  })).optional(),
   username: z.string()
 })
 const ResponseSchema = z.array(ContributorSchema).nonempty()
@@ -18,19 +23,24 @@ export async function fetchContributors ({ repoName }: { repoName: string }) {
     const queryParameters = new URLSearchParams({
       per_page: '3'
     })
-    const response = await fetch(`${apiURL(repoName)}?${queryParameters}`, {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: GH_API_TOKEN
-        // 'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
-    const responseData = await response.json()
+    const headers = {
+      Accept: 'application/vnd.github+json',
+      Authorization: GH_API_TOKEN
+    }
+    const repoResponse = await fetch(`${contributorsURL(repoName)}?${queryParameters}`, { headers })
+    const repoData = await repoResponse.json()
+    for (const contributor of repoData) {
+      const socialsResponse = await fetch(`${socialsURL(contributor.login)}`, { headers })
+      const socialsData = await socialsResponse.json()
+      contributor.socials = socialsData
+      console.debug('owo', socialsData)
+    }
 
-    // console.debug(responseData)
-    return ResponseSchema.parse(responseData?.map((contributor: any) => ({
+    console.debug('repo', repoData)
+    return ResponseSchema.parse(repoData?.map((contributor: any) => ({
       avatarUrl: contributor.avatar_url,
       contributions: contributor.contributions,
+      socialAccounts: contributor.socials,
       username: contributor.login
     })))
   } catch {
